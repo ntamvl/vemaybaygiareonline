10edb5a0a5b85ae43409cddc160f20f22fd1b7b0
Tam Nguyen <tamnt@zigexn.vn>
2015-05-07 17:16:40 +0700

    update

---
+++ wp-content/themes/fullby/page-tim-ve-may-bay-gia-re.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="row">
+<div class="col-sm-12 col-md-12">
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <h1>Searching</h1>
+      <iframe src="http://localhost:3000/booking/roundtrip/SGN/HAN/14-05-2015/19-05-2015/100" style="height:630px; width:100%; overflow:auto; border: 0;" ></iframe>
+      <!-- onload="window.parent.scrollTo(0,0)" allowtransparency="true" -->
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
