cd222fdce6af288218f210a9cc1d94f4f1463957
Tam Nguyen <tamnt@zigexn.vn>
2015-05-08 16:21:40 +0700

    update

--- wp-content/themes/fullby/page-tim-ve-may-bay-gia-re.php
+++ wp-content/themes/fullby/page-tim-ve-may-bay-gia-re.php
@@ -1,8 +1,20 @@
 <?php get_header(); ?>
+<?php
+  $trip_type = $_GET['trip_type'];
+  $departure = $_GET['departure'];
+  $destination = $_GET['destination'];
+  $from_date = $_GET['from_date'];
+  $to_date = $_GET['to_date'];
+  $adult = $_GET['adult'];
+  $children = $_GET['children'];
+  $infant = $_GET['infant'];
+  $search_url = "http://192.168.1.40:3000/booking/". $trip_type ."/". $departure ."/". $destination .
+                  "/". $from_date ."/". $to_date ."/" . $adult . $children . $infant . "&source=vemaygiareonline-net";
+?>
 
 <div id="container-iframe" class="row">
   <div class="col-sm-12 col-md-12 fill">
-    <iframe class="fill" src="http://localhost:3000/booking/roundtrip/SGN/HAN/14-05-2015/19-05-2015/100&source=vemaybay"></iframe>
+    <iframe class="fill" src="<?php echo $search_url; ?>"></iframe>
     <!-- onload="window.parent.scrollTo(0,0)" allowtransparency="true" -->
     <!-- <div class="panel panel-default fill">
         <div class="panel-body fill">
@@ -15,7 +27,7 @@
 <script type="text/javascript">
   jQuery(document).ready(function(){
     $(window).resize(function() {
-      jQuery('#container-iframe').height($(window).height() - 140);
+      jQuery('#container-iframe').height($(window).height() - 110);
     });
     $(window).trigger('resize');
   });
