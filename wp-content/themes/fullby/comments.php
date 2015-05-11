9a266cf9af1a86b96711c7f68ff6a826aa53fe92
Tam Nguyen <tamnt@zigexn.vn>
2015-05-11 10:48:31 +0700

    update comment

--- wp-content/themes/fullby/comments.php
+++ wp-content/themes/fullby/comments.php
@@ -7,7 +7,7 @@ if ( post_password_required() ) { ?>
 <?php
     return; }
 // ##########  End do not delete section
- 
+
 // Display Comments Section
 if ( have_comments() ) : ?>
     <h3 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses');?> </h3>
@@ -37,7 +37,7 @@ if ( have_comments() ) : ?>
     if ( ! comments_open() ) : // There are comments but comments are now closed
         echo"<p class='nocomments'>Comments are closed.</p>";
     endif;
- 
+
 else : // I.E. There are no Comments
     if ( comments_open() ) : // Comments are open, but there are none yet
         // echo"<p>Be the first to write a comment.</p>";
@@ -45,20 +45,41 @@ else : // I.E. There are no Comments
         echo"<p class='nocomments'>Comments are closed.</p>";
     endif;
 endif;
- 
+
+$fields =  array(
+
+  'author' =>
+    '<p class="comment-form-author"><label for="author">' . __( 'Họ tên', 'domainreference' ) . '</label> ' .
+    ( $req ? '<span class="required">*</span>' : '' ) .
+    '<input id="author" name="author" type="text" class="form-control" value="' . esc_attr( $commenter['comment_author'] ) .
+    '" size="30"' . $aria_req . ' /></p>',
+
+  'email' =>
+    '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+    ( $req ? '<span class="required">*</span>' : '' ) .
+    '<input id="email" name="email" type="text" class="form-control" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+    '" size="30"' . $aria_req . ' /></p>',
+
+  // 'url' =>
+  //   '<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+  //   '<input id="url" name="url" class="form-control" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+  //   '" size="30" /></p>',
+);
+
 // Display Form/Login info Section
 // the comment_form() function handles this and can be used without any paramaters simply as "comment_form()"
 comment_form(array(
   // see codex http://codex.wordpress.org/Function_Reference/comment_form for default values
   // tutorial here http://blogaliving.com/wordpress-adding-comment_form-theme/
-  'comment_field' => '<p><textarea name="comment" id="comment" cols="58" rows="10" tabindex="4" aria-required="true"></textarea></p>',
-  'label_submit' => 'Submit Comment',
-  'comment_notes_after' => ''
+  'comment_field' => '<p><label for="comment">' . __( 'Nội dung', 'domainreference' ) . '</label> <textarea name="comment" id="comment" class="form-control" cols="58" rows="10" tabindex="4" aria-required="true"></textarea></p>',
+  'label_submit' => 'Gửi ',
+  'comment_notes_after' => '',
+  'fields' => apply_filters( 'comment_form_default_fields', $fields ),
   ));
- 
+
 // RSS comments link
 echo '<div class="comments_rss">';
-post_comments_feed_link('Comments RSS Feed');
+post_comments_feed_link('RSS Feed');
 echo '</div>';
- 
+
 ?>
