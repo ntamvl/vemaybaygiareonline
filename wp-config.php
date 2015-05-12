cfbddedb852b64a109d3bf234150c3a312bf220c
Tam Nguyen <tamnt@zigexn.vn>
2015-05-12 14:44:55 +0700

    add cache

--- wp-config.php
+++ wp-config.php
@@ -28,7 +28,7 @@ define('DB_PASSWORD', 'nguyen');
 define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
