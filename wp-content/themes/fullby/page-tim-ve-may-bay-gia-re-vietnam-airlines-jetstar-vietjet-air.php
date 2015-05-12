342719bb6b91851f3317ecfa470ba08fcd69a755
Tam Nguyen <tamnt@zigexn.vn>
2015-05-12 16:50:24 +0700

    update

--- wp-content/themes/fullby/page-tim-ve-may-bay-gia-re-vietnam-airlines-jetstar-vietjet-air.php
+++ wp-content/themes/fullby/page-tim-ve-may-bay-gia-re-vietnam-airlines-jetstar-vietjet-air.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
+<div class="row featured">
+  <div class="col-sm-12 col-md-12 item-featured">
+    <div class="panel panel-default panel-search-box">
+      <div class="panel-body">
+        <?php get_template_part( 'search-box' ); ?>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div class="col-md-12 cont-grid">
 
 </div>
