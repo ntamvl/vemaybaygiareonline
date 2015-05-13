6ee4648d007d2e0512f231639bdd8332f6bd0ca6
Tam Nguyen <tamnt@zigexn.vn>
2015-05-13 09:10:04 +0700

    update page search form

--- wp-content/themes/fullby/page-tim-ve-may-bay-gia-re-vietnam-airlines-jetstar-vietjet-air.php
+++ wp-content/themes/fullby/page-tim-ve-may-bay-gia-re-vietnam-airlines-jetstar-vietjet-air.php
@@ -5,7 +5,7 @@
 
 </div> -->
 
-<div class="col-md-12 cont-grid">
+<div class="col-md-12 cont-grid" id="search-box-container">
   <div class="panel panel-default panel-search-box">
     <div class="panel-body">
       <?php get_template_part( 'search-box' ); ?>
@@ -13,5 +13,16 @@
   </div>
   <br/>
 </div>
+
+<script type="text/javascript">
+  jQuery(document).ready(function(){
+    $(window).resize(function() {
+      // jQuery('#search-box-container').height($(window).height() - $('.navbar').outerHeight() - $('.footer').outerHeight() - 80);
+    });
+    $(window).trigger('resize');
+    jQuery('.footer').addClass('enclose');
+
+  });
+</script>
 
 <?php get_footer(); ?>
