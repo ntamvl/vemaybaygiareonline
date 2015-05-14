aff716dbfce9c778c463dfeca0779a85fab5cf36
Tam Nguyen <tamnt@zigexn.vn>
2015-05-14 12:31:47 +0700

    add new theme update from old

---
+++ wp-content/themes/vemaybay_od1/search-box-sidebar.php
@@ -0,0 +1,76 @@
+<form method="get" action="/tim-ve-may-bay-gia-re/">
+<div class="wapper-search-home">
+  <div class="wapper-search2">
+    <div class="col-md-12">
+      <h2 class="title-search">Đặt vé máy bay giá rẻ ngay</h2>
+    </div>
+    <div class="row">
+      <div class="col-md-6 height-radio col-xs-6">
+        <div class="radio text-chon ">
+          <label>
+            <input type="radio" name="trip_type" value="roundtrip" checked><span class="title-search-mini">Khứ hồi</span>
+          </label>
+        </div>
+      </div>
+      <div class="col-md-6 height-radio col-xs-6">
+        <div class="radio text-chon">
+          <label>
+            <input type="radio" name="trip_type"  value="oneway"><span class="title-search-mini">Một chiều</span>
+          </label>
+        </div>
+      </div>
+    </div>
+    <div class="row margin-top-form">
+      <div class="col-md-12">
+        <span class="title-search-mini">Điểm khởi hành</span>
+        <div class="">
+         <select id="select2-airport-code" class="form-control style-form2 populate placeholder location-choose departure-search" name="departure">
+           <option value="SGN">Hồ Chí Minh  (SGN)</option>
+         </select>
+       </div>
+     </div>
+     <div class="col-md-12">
+      <span class="title-search-mini">Điểm đến</span>
+      <div class="">
+        <select id="select2-airport-code" class="form-control style-form2 populate placeholder location-choose destination-search" name="destination">
+          <option value="HAN">Hà Nội (HAN)</option>
+        </select>
+      </div>
+    </div>
+    </div>
+    <div class="row margin-top-form">
+      <div class="col-md-12">
+        <span class="title-search-mini">Ngày đi</span>
+        <div class="wap-field-form">
+          <input type="text" class="form-control style-form2 from_date" name="from_date" />
+        </div>
+      </div>
+      <div class="col-md-12">
+        <span class="title-search-mini">Ngày về</span>
+        <div class="wap-field-form">
+          <input type="text" class="form-control style-form2 to_date" name="to_date">
+        </div>
+      </div>
+    </div>
+    <div class="col-md-12"><span class="title-search-mini">Người lớn</span> (>12 tuổi)
+      <select class="form-control style-form" name="adult" id="adult"></select>
+    </div>
+    <div class="col-md-12"><span class="title-search-mini">Trẻ em</span> (2 - 12 tuổi)
+      <select class="form-control style-form" name="children" id="child"></select>
+    </div>
+    <div class="col-md-12"><span class="title-search-mini">Em bé </span> (< 2 tuổi)
+      <select class="form-control style-form" name="infant" id="infant"></select>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 min-2">
+        <button type="submit" id="search" name="search" class="btn btn-default btn-search btn-search-home col-md-12 col-xs-12"><span class="glyphicon glyphicon-search"></span>
+          Tìm
+        </button>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+</form>
