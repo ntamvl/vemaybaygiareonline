43b440a684eb5dfa0212cbeda15ac14e957b3aa9
Tam Nguyen <tamnt@zigexn.vn>
2015-05-14 15:50:29 +0700

    add logo

--- wp-content/themes/vemaybay_od2/header.php
+++ wp-content/themes/vemaybay_od2/header.php
@@ -52,7 +52,7 @@
           <h1><a class="navbar-brand" href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>">
             <!-- <i class="fa fa-chevron-right"></i><i class="fa fa-chevron-right arrow"></i> -->
               <?php //bloginfo('name'); ?>
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" style="height: 50px; width: auto;">
+              <img class="img-responsive"> src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" style="height: 50px; width: auto;">
             </a>
           </h1>
         </div>
