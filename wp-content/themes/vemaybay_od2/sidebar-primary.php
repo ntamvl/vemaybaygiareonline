6076ed6081a4997586e2b43753f2d109f6040cd9
Tam Nguyen <tamnt@zigexn.vn>
2015-05-18 13:41:54 +0700

    update most views

--- wp-content/themes/vemaybay_od2/sidebar-primary.php
+++ wp-content/themes/vemaybay_od2/sidebar-primary.php
@@ -30,6 +30,7 @@
 
 			<li class="active"><a href="#home" data-toggle="tab"> <i class="fa fa-bolt"></i> Tin khuyến mãi</a></li>
 			<li><a href="#profile" data-toggle="tab"> <i class="fa fa-clock-o"></i> Tin mới nhất</a></li>
+			<li><a href="#top-views" data-toggle="tab"> <i class="fa fa-clock-o"></i> Xem nhiều nhất</a></li>
 
 		</ul>
 
@@ -96,6 +97,7 @@
 
 			</div>
 
+			<!-- begin lastest posts -->
 			<div class="tab-pane fade" id="profile">
 
 		  		<?php
@@ -147,6 +149,67 @@
 				<?php endwhile; ?>
 
 			</div>
+			<!-- end lastest posts -->
+
+			<!-- begin most view -->
+			<div class="tab-pane fade" id="top-views">
+
+	  		<?php
+	  		$top_views_query = array(
+													'posts_per_page' => 5,
+													'meta_key' => 'wpb_post_views_count',
+													'orderby' => 'meta_value_num',
+													'order' => 'DESC'
+												);
+				$popularpost = new WP_Query( $top_views_query );
+				while ( $popularpost->have_posts() ) : $popularpost->the_post();?>
+
+					<a href="<?php the_permalink(); ?>">
+
+						<?php $video = get_post_meta($post->ID, 'fullby_video', true );
+
+						if($video != '') {?>
+
+							<img src="http://img.youtube.com/vi/<?php echo $video ?>/1.jpg" class="grid-cop"/>
+
+						<?php
+
+		             	} else if ( has_post_thumbnail() ) { ?>
+
+	                        <?php the_post_thumbnail('thumbnail', array('class' => 'thumbnail')); ?>
+
+	                    <?php } ?>
+
+			    		<h2 class="title"><?php the_title(); ?></h2>
+
+			    		<div class="date"><i class="fa fa-clock-o"></i> <?php the_time('d/m/Y') ?> &nbsp;
+			    			<span class="post-view">
+			    				<i class="fa fa-eye"></i>
+			    				<?php echo get_post_meta( get_the_ID(), 'wpb_post_views_count', true ); ?> lượt xem</span>
+
+							<?php
+							$video = get_post_meta($post->ID, 'fullby_video', true );
+
+							if($video != '') { ?>
+
+			             		<i class="fa fa-video-camera"></i> Video
+
+			             	<?php } else if (strpos($post->post_content,'[gallery') !== false) { ?>
+
+			             		<i class="fa fa-th"></i> Gallery
+
+		             		<?php } else {?>
+
+		             		<?php } ?>
+
+			    		</div>
+
+			    	</a>
+
+				<?php endwhile; ?>
+
+			</div>
+			<!-- end most view -->
 
 		</div>
 
