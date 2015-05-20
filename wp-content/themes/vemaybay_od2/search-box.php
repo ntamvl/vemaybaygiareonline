a1bb28e9363efaea705c40a1a883a8422202d4ec
Tam Nguyen <tamnt@zigexn.vn>
2015-05-20 10:29:42 +0700

    update title

--- wp-content/themes/vemaybay_od2/search-box.php
+++ wp-content/themes/vemaybay_od2/search-box.php
@@ -2,7 +2,7 @@
 <div class="wapper-search-home">
   <div class="wapper-search2">
     <div class="col-md-12">
-      <h2 class="title-search">Tìm vé máy bay giá rẻ ngay</h2>
+      <h2 class="title-search">Tìm vé máy bay giá rẻ</h2>
     </div>
     <div class="row">
       <div class="col-md-6 height-radio col-xs-6">
